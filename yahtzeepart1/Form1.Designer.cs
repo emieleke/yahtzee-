a597345a6365b815315f60349313598d0699e988
emieleke <0321551@student.rocvantwente.nl>
2020-01-21 14:41:48 +0100

    part 1 af

---
+++ yahtzeepart1/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace yahtzeepart1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dobbelen = new System.Windows.Forms.Button();
+            this.spelregels = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.checkBox_stop3 = new System.Windows.Forms.CheckBox();
+            this.checkBox_stop4 = new System.Windows.Forms.CheckBox();
+            this.checkBox_stop2 = new System.Windows.Forms.CheckBox();
+            this.checkBox_stop5 = new System.Windows.Forms.CheckBox();
+            this.checkBox_stop1 = new System.Windows.Forms.CheckBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.score_textbox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bonus_textbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dobbelen
+            // 
+            this.dobbelen.BackColor = System.Drawing.Color.DarkGray;
+            this.dobbelen.Location = new System.Drawing.Point(279, 444);
+            this.dobbelen.Name = "dobbelen";
+            this.dobbelen.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dobbelen.Size = new System.Drawing.Size(307, 59);
+            this.dobbelen.TabIndex = 1;
+            this.dobbelen.Text = "dobbelen";
+            this.dobbelen.UseVisualStyleBackColor = false;
+            this.dobbelen.Click += new System.EventHandler(this.dobbelen_Click);
+            // 
+            // spelregels
+            // 
+            this.spelregels.BackColor = System.Drawing.Color.Transparent;
+            this.spelregels.Location = new System.Drawing.Point(23, 26);
+            this.spelregels.Name = "spelregels";
+            this.spelregels.Size = new System.Drawing.Size(97, 36);
+            this.spelregels.TabIndex = 7;
+            this.spelregels.Text = "spelregels";
+            this.spelregels.UseVisualStyleBackColor = false;
+            this.spelregels.Click += new System.EventHandler(this.spelregels_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::yahtzeepart1.Properties.Resources._120px_Dice_0;
+            this.pictureBox1.Location = new System.Drawing.Point(110, 260);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 140);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::yahtzeepart1.Properties.Resources._120px_Dice_0;
+            this.pictureBox4.Location = new System.Drawing.Point(514, 114);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 140);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = global::yahtzeepart1.Properties.Resources._120px_Dice_0;
+            this.pictureBox5.Location = new System.Drawing.Point(651, 260);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(150, 145);
+            this.pictureBox5.TabIndex = 13;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::yahtzeepart1.Properties.Resources._120px_Dice_0;
+            this.pictureBox2.Location = new System.Drawing.Point(242, 114);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 140);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::yahtzeepart1.Properties.Resources._120px_Dice_0;
+            this.pictureBox3.Location = new System.Drawing.Point(372, 260);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 140);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // checkBox_stop3
+            // 
+            this.checkBox_stop3.AutoSize = true;
+            this.checkBox_stop3.Location = new System.Drawing.Point(439, 406);
+            this.checkBox_stop3.Name = "checkBox_stop3";
+            this.checkBox_stop3.Size = new System.Drawing.Size(18, 17);
+            this.checkBox_stop3.TabIndex = 19;
+            this.checkBox_stop3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_stop4
+            // 
+            this.checkBox_stop4.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox_stop4.Location = new System.Drawing.Point(576, 260);
+            this.checkBox_stop4.Name = "checkBox_stop4";
+            this.checkBox_stop4.Size = new System.Drawing.Size(23, 24);
+            this.checkBox_stop4.TabIndex = 24;
+            this.checkBox_stop4.UseVisualStyleBackColor = false;
+            // 
+            // checkBox_stop2
+            // 
+            this.checkBox_stop2.AutoSize = true;
+            this.checkBox_stop2.Location = new System.Drawing.Point(308, 260);
+            this.checkBox_stop2.Name = "checkBox_stop2";
+            this.checkBox_stop2.Size = new System.Drawing.Size(18, 17);
+            this.checkBox_stop2.TabIndex = 21;
+            this.checkBox_stop2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_stop5
+            // 
+            this.checkBox_stop5.AutoSize = true;
+            this.checkBox_stop5.Location = new System.Drawing.Point(717, 412);
+            this.checkBox_stop5.Name = "checkBox_stop5";
+            this.checkBox_stop5.Size = new System.Drawing.Size(18, 17);
+            this.checkBox_stop5.TabIndex = 22;
+            this.checkBox_stop5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_stop1
+            // 
+            this.checkBox_stop1.AutoSize = true;
+            this.checkBox_stop1.Location = new System.Drawing.Point(178, 406);
+            this.checkBox_stop1.Name = "checkBox_stop1";
+            this.checkBox_stop1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox_stop1.TabIndex = 23;
+            this.checkBox_stop1.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // score_textbox
+            // 
+            this.score_textbox.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.score_textbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.score_textbox.Font = new System.Drawing.Font("Algerian", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score_textbox.Location = new System.Drawing.Point(778, 25);
+            this.score_textbox.Multiline = true;
+            this.score_textbox.Name = "score_textbox";
+            this.score_textbox.ReadOnly = true;
+            this.score_textbox.Size = new System.Drawing.Size(200, 50);
+            this.score_textbox.TabIndex = 25;
+            this.score_textbox.Text = "Score:";
+            this.score_textbox.TextChanged += new System.EventHandler(this.score_textbox_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Algerian", 28.2F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(969, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 63);
+            this.textBox1.TabIndex = 26;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // bonus_textbox
+            // 
+            this.bonus_textbox.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.bonus_textbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.bonus_textbox.Font = new System.Drawing.Font("Algerian", 28.2F, System.Drawing.FontStyle.Bold);
+            this.bonus_textbox.Location = new System.Drawing.Point(778, 453);
+            this.bonus_textbox.Multiline = true;
+            this.bonus_textbox.Name = "bonus_textbox";
+            this.bonus_textbox.ReadOnly = true;
+            this.bonus_textbox.Size = new System.Drawing.Size(298, 50);
+            this.bonus_textbox.TabIndex = 1;
+            this.bonus_textbox.TextChanged += new System.EventHandler(this.bonus_textbox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1088, 555);
+            this.Controls.Add(this.bonus_textbox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.score_textbox);
+            this.Controls.Add(this.checkBox_stop1);
+            this.Controls.Add(this.checkBox_stop5);
+            this.Controls.Add(this.checkBox_stop2);
+            this.Controls.Add(this.checkBox_stop4);
+            this.Controls.Add(this.checkBox_stop3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.spelregels);
+            this.Controls.Add(this.dobbelen);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "yathzee  (chris, emiel, emre, dylan, mike)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button dobbelen;
+        private System.Windows.Forms.Button spelregels;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.CheckBox checkBox_stop3;
+        private System.Windows.Forms.CheckBox checkBox_stop4;
+        private System.Windows.Forms.CheckBox checkBox_stop2;
+        private System.Windows.Forms.CheckBox checkBox_stop5;
+        private System.Windows.Forms.CheckBox checkBox_stop1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox score_textbox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox bonus_textbox;
+    }
+}
+
---
+++ yahtzeepart1/Form1.cs
@@ -0,0 +1,200 @@
+﻿#region using statements
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#endregion
+
+namespace yahtzeepart1
+{
+    public partial class Form1 : Form
+    {
+
+
+        Image[] diceimages;
+        int[] dice;
+        Random rand;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //alle plaatjes voor de dobbelstenen
+            diceimages = new Image[7];
+            diceimages[0] = Properties.Resources._120px_Dice_0;
+            diceimages[1] = Properties.Resources.Alea_1;
+            diceimages[2] = Properties.Resources.Alea_2;
+            diceimages[3] = Properties.Resources.Alea_3;
+            diceimages[4] = Properties.Resources.Alea_4;
+            diceimages[5] = Properties.Resources.Alea_5;
+            diceimages[6] = Properties.Resources.Alea_6;
+
+            dice = new int[5] { 0, 0, 0, 0, 0, };
+
+            rand = new Random();
+
+
+        }
+
+
+
+
+        public void dobbelen_Click(object sender, EventArgs e)
+        {
+            Rolldice();
+
+            Array.Sort(dice);
+            //berekend grote straat 
+            if (dice[0] == 1 && dice[1] == 2 && dice[2] == 3 && dice[3] == 4 && dice[4] == 5 || dice[0] == 2 && dice[1] == 3 && dice[2] == 4 && dice[3] == 5 && dice[4] == 6)
+            {
+                bonus_textbox.Text = "grote straat";
+            }
+            else
+            {
+                bonus_textbox.Text = "";
+            }
+            //berekend kleine straat 
+            if (dice[0] == 1 && dice[1] == 2 && dice[2] == 3 && dice[3] == 4 || dice[1] == 2 && dice[2] == 3 && dice[3] == 4 && dice[4] == 5)
+            {
+                bonus_textbox.Text = "kleine straat";
+            }
+            else
+            {
+                bonus_textbox.Text = "";
+            }
+            //berekend Full House
+            if (dice[0] == 1 && dice[1] == 2 && dice[2] == 3 && dice[3] == 4 && dice[2] != dice[3] || dice[0] == 1 && dice[1] == 2 && dice[2] == 3 && dice[3] == 4 && dice[1] != dice[2])
+            {
+                bonus_textbox.Text = "full house";
+            }
+            else
+            {
+                bonus_textbox.Text = "";
+            }
+
+            int dobbel1 = (dice[0]);
+            int dobbel2 = (dice[1]);
+            int dobbel3 = (dice[2]);
+            int dobbel4 = (dice[3]);
+            int dobbel5 = (dice[4]);
+            int score = dobbel1 + dobbel2 + dobbel3 + dobbel4 + dobbel5;
+            textBox1.Text = Convert.ToString(score);
+
+
+            if (dobbel1 == dobbel2 && dobbel2 == dobbel3 && dobbel3 == dobbel4 && dobbel4 == dobbel5)
+            {
+                bonus_textbox.Text = "yahtzee!";
+            }
+            else
+            {
+                bonus_textbox.Text = "";
+            }
+            if (dobbel1 == dobbel2 && dobbel2 == dobbel3 || dobbel3 == dobbel4 && dobbel4 == dobbel5)
+            {
+                bonus_textbox.Text = "drie gelijke";
+            }
+            else
+            {
+                bonus_textbox.Text = "";
+            }
+            if (dobbel1 == dobbel2 && dobbel2 == dobbel3 && dobbel3 == dobbel4 || dobbel2 == dobbel3 && dobbel3 == dobbel4 && dobbel4 == dobbel5)
+            {
+                bonus_textbox.Text = "vier gelijke";
+            }
+            else
+            {
+                bonus_textbox.Text = "";
+            }
+        }
+
+        //dit veranderd de plaatjes
+        private void Rolldice()
+        {
+            for (int i = 0; i < dice.Length; i++)
+                dice[i] = rand.Next(1, 7);
+
+
+
+            pictureBox1.Image = diceimages[dice[0]];
+            pictureBox2.Image = diceimages[dice[1]];
+            pictureBox3.Image = diceimages[dice[2]];
+            pictureBox4.Image = diceimages[dice[3]];
+            pictureBox5.Image = diceimages[dice[4]];
+        }
+
+
+
+        //spelregel knop 
+        private void spelregels_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Spelregels Yahtzee \nHoe werkt het? \nHet spel Yahtzee is een dobbelspel dat gespeeld wordt met 5 dobbelstenen en een scoreblok. Elke speler mag per beurt maximaal 3 x met de dobbelstenen gooien en proberen een zo hoog mogelijke score te behalen die hij kan opschrijven bij een van de combinaties op het scoreblok. Je bent verplicht om na elke beurt een vakje in te kruisen. \n                                                                                                                         Scores:\nBovenste vakken Bij de vakken 1 t / m 6 is het aantal punten gelijk aan het aantal keer dat een dobbelsteen het overeenkomstige aantal ogen aanwijst, vermenigvuldigd met het aantal ogen.\nOnderste vakken:\nDrie gelijke: De score is het totaal van alle ogen, als er minstens 3 dobbelstenen met hetzelfde aantal ogen zijn.\nVier gelijke: De score is het totaal van alle ogen, als er minstens 4 dobbelstenen met hetzelfde aantal ogen zijn.\nleine straat: 30 punten voor 4 oplopende dobbelstenen. (de volgorde speelt geen rol)\nGrote straat: 40 punten voor 5 oplopende dobbelstenen. (de volgorde speelt geen rol)\nFull House: 25 punten voor 3 gelijke en één paar. (5 gelijke telt niet als Full House, tenzij het vak Yahtzee reeds ingevuld is).\nKans: De score is het totaal aantal ogen van alle dobbelstenen.\nYahtzee: 50 punten als alle dobbelstenen hetzelfde aantal ogen hebben.");
+        }
+
+
+     
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //hier hoeven we niks mee ik kan ze gewoon niet wegkrijgen
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void score_textbox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void bonus_textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
